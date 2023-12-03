c18ef70a2ac570654c11a2c15419e828244dc417
jeff-butler-dev <130912131+jeff-butler-dev@users.noreply.github.com>
2023-12-03 12:28:52 -0600

    formatting changes in exit.js

--- commands/exit.js
+++ commands/exit.js
@@ -1,20 +1,19 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { useQueue } = require('discord-player');
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { useQueue } = require("discord-player");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-            .setName('exit')
-            .setDescription('removes bot from channel'),
-        execute: async ({client, interaction}) => {
+  data: new SlashCommandBuilder()
+    .setName("exit")
+    .setDescription("removes bot from channel"),
+  execute: async ({ client, interaction }) => {
+    const queue = useQueue(interaction.guildId);
 
-            const queue = useQueue(interaction.guildId);
+    if (!queue.connection) {
+      await interaction.reply("Not in a channel to exit");
+      return;
+    }
+    queue.connection.destroy();
 
-            if(!queue.connection) {
-                await interaction.reply('Not in a channel to exit');
-                return
-            }
-            queue.connection.destroy()
-
-            await interaction.reply('Successfully removed')
-        }
-}
+    await interaction.reply("Successfully removed");
+  },
+};
